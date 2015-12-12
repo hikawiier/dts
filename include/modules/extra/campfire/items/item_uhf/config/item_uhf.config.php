bd92d13ffc678192f956de5a664d0c0e8caf4ac3
hikawiier <hikawiier@foxmail.com>
2015-12-12 15:57:21 +0800

    fix bug bancombo,item_uhf
    修复了限定解除和特殊补给的BUG

---
+++ include/modules/extra/campfire/items/item_uhf/config/item_uhf.config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace item_uhf
+{
+	$buff_kind_array = Array(
+		2900 => Array(
+			'kind' => Array('msp','sp','att','def'),
+			'lasttime' => 15,
+		),
+	);
+}
+
+?>
--- include/modules/extra/campfire/items/item_uhf/module.inc.php
+++ include/modules/extra/campfire/items/item_uhf/module.inc.php
@@ -6,7 +6,7 @@ namespace item_uhf
 	$___MODULE_dependency = 'sys player itemmain logger';
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
-	$___MODULE_codelist = 'main.php';
+	$___MODULE_codelist = 'main.php config/item_uhf.config.php';
 	$___MODULE_templatelist = '';
 	////////// MODULE HEADER END /////////////////
 	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
