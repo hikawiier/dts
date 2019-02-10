d7c962813a768d08e4d8149e55edbb2ebba671ff
7 <hikawiier@foxmail.com>
2019-02-10 13:14:19 +0800

    他改变了合成
    他又给改回去了

--- include/modules/base/itemmix/itemmix/config/itemmix.config.php
+++ include/modules/base/itemmix/itemmix/config/itemmix.config.php
@@ -415,25 +415,23 @@ $mixinfo = array
 	array('class' => 'hidden', 'stuff' => array('黑色碎片','十字发卡'),'result' => array('黑色发卡','X',1,1,),),
 	array('class' => 'hidden', 'stuff' => array('『T.E.R.R.A』','『A.Q.U.A』','『V.E.N.T.U.S』'),'result' => array('『G.A.M.E.O.V.E.R』','Y',1,1,),),
 	array('class' => 'hidden', 'stuff' => array('提示纸条K','提示纸条E'),'result' => array('提示纸条Y','WC',1,1,'zd'),),
-	array('class' => 'hidden', 'stuff' => array('【红杀铁剑】','电磁充能手套'),'result' => array('【红杀铁剑·雷击】','WK',300,45,'ned'),),
-
-	//为了便于维护，将MOD增加的合成列表移至下方。期待有更独立的解决方法。
-	//篝火-回复道具
-	array('class' => 'h', 'stuff' => array('压缩饼干','水'),'result' => array('应急储备粮','HB',95,4,),),
-	array('class' => 'h', 'stuff' => array('压缩饼干','蒸馏水'),'result' => array('健康储备粮','HB',150,4,),),
-	array('class' => 'h', 'stuff' => array('食堂的盒饭','打火机'),'result' => array('迷之黑暗物质','PR',100,5,),),
-	//篝火-钝器：
-	array('class' => 'wp', 'stuff' => array('每只虾360元的炸虾饭','黑色方块','白色方块','冰棍棒'),'result' => array('★悔悟之棒★','WP',1514,'∞','Znrk'),),
-	array('class' => 'hidden', 'stuff' => array('每只虾360元的炸虾饭','黑色方块','白色方块','冰钉棍棒'),'result' => array('★悔悟之棒★','WP',1514,'∞','Znrk'),),
-	//篝火-爆系武器：
-	array('class' => 'wd', 'stuff' => array('★BIUBIUBIU★','悲叹之种','蒸馏水'),'result' => array('★冰枪术★','WD',666,'∞','ZnkNdr'),),
-	//篝火-道具：
-	array('class' => 'item', 'stuff' => array('打火机','地雷'),'result' => array('土制二踢脚（？）','EW',1,1,0),),
-	//篝火-方块系：
-	array('class' => 'cube', 'stuff' => array('黑色方块','白色方块','怨灵'),'result' => array('★无极★','WP',114,514,'Nrd'),),
-	array('class' => 'cube', 'stuff' => array('黑色方块','白色方块','幽灵'),'result' => array('★无极★','WP',114,514,'Nrd'),),
+	array('class' => 'hidden', 'stuff' => array('【红杀铁剑】','电磁充能手套'),'result' => array('【红杀铁剑·雷击】','WK',300,45,'ned'),),	
 	);
-	
+	//为了便于维护，将MOD增加的合成列表移至$mixinfo外。期待有更独立的解决方法。
+	//篝火-回复道具
+	$mixinfo[]=array('class' => 'h', 'stuff' => array('压缩饼干','水'),'result' => array('应急储备粮','HB',95,4,),);
+	$mixinfo[]=array('class' => 'h', 'stuff' => array('压缩饼干','蒸馏水'),'result' => array('健康储备粮','HB',150,4,),);
+	$mixinfo[]=array('class' => 'h', 'stuff' => array('食堂的盒饭','打火机'),'result' => array('迷之黑暗物质','PR',100,5,),);
+	//篝火-钝器：
+	$mixinfo[]=array('class' => 'wp', 'stuff' => array('每只虾360元的炸虾饭','黑色方块','白色方块','冰棍棒'),'result' => array('★悔悟之棒★','WP',1514,'∞','Znrk'),);
+	$mixinfo[]=array('class' => 'hidden', 'stuff' => array('每只虾360元的炸虾饭','黑色方块','白色方块','冰钉棍棒'),'result' => array('★悔悟之棒★','WP',1514,'∞','Znrk'),);
+	//篝火-爆系武器：
+	$mixinfo[]=array('class' => 'wd', 'stuff' => array('★BIUBIUBIU★','悲叹之种','蒸馏水'),'result' => array('★冰枪术★','WD',666,'∞','ZnkNdr'),);
+	//篝火-道具：
+	$mixinfo[]=array('class' => 'item', 'stuff' => array('打火机','地雷'),'result' => array('土制二踢脚（？）','EW',1,1,0),);
+	//篝火-方块系：
+	$mixinfo[]=array('class' => 'cube', 'stuff' => array('黑色方块','白色方块','怨灵'),'result' => array('★无极★','WP',114,514,'Nrd'),);
+	$mixinfo[]=array('class' => 'cube', 'stuff' => array('黑色方块','白色方块','幽灵'),'result' => array('★无极★','WP',114,514,'Nrd'),);
 }
 
 ?>
