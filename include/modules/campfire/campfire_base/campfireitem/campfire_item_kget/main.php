93100e1335c6b951bb89511cee5b7ebc846e448c
7 <hikawiier@foxmail.com>
2019-02-23 18:26:43 +0800

    更新自动针线

--- include/modules/campfire/campfire_base/campfireitem/campfire_item_kget/main.php
+++ include/modules/campfire/campfire_base/campfireitem/campfire_item_kget/main.php
@@ -18,6 +18,8 @@ namespace campfire_item_kget
 				$ret .= '幻境控制系统的移动子端，可解除禁区或使禁区提前到来，且内置最高级的电子雷达';
 			}elseif ($n == '再启动指令集') {
 				$ret .= '使用后能达到使游戏重新开始的效果，但需要满足一定的条件';
+			}elseif ($n == '迷你纺织者「睡美人」') {
+				$ret .= '可以增加身体防具的效果值，需要消耗针线包填充';
 			}
 		}
 		return $ret;
@@ -72,7 +74,6 @@ namespace campfire_item_kget
 		}
 		$log.="当你提交了操作后，看起来只有巴掌大的方盒忽然从中间展开，<br>许多细微的金属丝从中弹射而出，将你的动力装甲包裹其中。<br>漫长的修复作业开始了……<br>";
 		$log.="……<br>";
-		$ritm['itme'] = round($ritm['itme'] * rand(8,10) / 10);
 		${$kind.'s'} = ${$kind.'s'}=='∞' ? $ritm['itme'] : ${$kind.'s'}+$ritm['itme'];
 		$log.="不知过了多久，伴随着一声清脆的提示音，你看到那些金属丝以肉眼难辨的速度回到了方盒内。<br>当你低头查看时，你才发现，自己的动力装甲已经焕然一新了。<br>你的{${$kind}}增加了<span class='yellow'>{$ritm['itme']}</span>点耐久！<br>";
 		$ritm['itme'] = 0;
@@ -112,7 +113,8 @@ namespace campfire_item_kget
 						$mitm['itm']=&${'itm'.$i}; $mitm['itmk']=&${'itmk'.$i};
 						$mitm['itme']=&${'itme'.$i}; $mitm['itms']=&${'itms'.$i}; $mitm['itmsk']=&${'itmsk'.$i};
 						$metal_flag += $mitm['itme']*$mitm['itms'];	
-						\itemmain\itms_reduce($mitm,1);
+						$mitm['itms']=1;
+						\itemmain\itms_reduce($mitm);
 						break;
 					}
 				}
@@ -120,11 +122,11 @@ namespace campfire_item_kget
 				{
 					$metal_flag = round($metal_flag * rand(8,10) / 10);
 					$itme += $metal_flag;
-					$log.="你打开了修复工具的装填仓，将金属材料放入其中，你隐约能听到小盒子内传出机械齿轮转动的声音。<br>修复工具的材料储量增加了<span class='yellow'>{$metal_flag}</span>点！<br>";
+					$log.="你打开了修复工具的盒盖，将金属材料放入其中。<br>隐约能听到从小盒子内传出齿轮咬合的声音。<br>装填工作开始了。<br>……<br>修复工具的材料储量增加了<span class='yellow'>{$metal_flag}</span>点！<br>";
 				}
 				else
 				{
-					$log.="修复工具的装填仓为空，而且你身上没有金属片！<br>";
+					$log.="修复工具需要装填素材才能工作，去找点金属材料吧！<br>";
 					return;
 				}
 			}
@@ -169,6 +171,52 @@ namespace campfire_item_kget
 				$gamestate = 40;
 				addnews($time,'combo');
 				save_gameinfo();
+			}
+			return;
+		}
+		if($itm=='迷你纺织者「睡美人」')
+		{
+			//判定是否填充针线
+			if($itme==0)
+			{
+				$reloading_effects=0;
+				for($i=0;$i<=6;$i++)
+				{
+					if(${'itms'.$i} && strpos(${'itm'.$i},'针线包')!==false)
+					{
+						$mitm['itm']=&${'itm'.$i}; $mitm['itmk']=&${'itmk'.$i};
+						$mitm['itme']=&${'itme'.$i}; $mitm['itms']=&${'itms'.$i}; $mitm['itmsk']=&${'itmsk'.$i};
+						$reloading_effects += $mitm['itme']*$mitm['itms'];	
+						$mitm['itms'] = 1;
+						\itemmain\itms_reduce($mitm);
+						break;
+					}				
+				}
+				if(!$reloading_effects)
+				{
+					$log .= '纺织者需要素材才能工作，去找点针线包来吧！<br>';
+					return;
+				}
+				else
+				{
+					$reloading_effects = round($reloading_effects * rand(8,10) / 10);
+					$itme += $reloading_effects;
+					$log.="你打开了纺织者的盖子，将针线放入其中。<br>隐约能听到从纺织者体内传出齿轮咬合的声音。<br>装填工作开始了。<br>……<br>纺织者的素材储量增加了<span class='yellow'>{$reloading_effects}</span>点！<br>";
+				}	
+			}
+			//开始快速打针线包
+			else
+			{
+				if (($arb == $noarb) || ! $arb) 
+				{
+					$log .= '你没有装备防具，不能使用纺织者。<br>';
+				} 
+				else 
+				{
+					$arbe += $itme;
+					$log .= "你启动了纺织者。<br>你眼睁睁地看着这位“睡美人”从中轴上裂开，像张开的大嘴一般将你的防具一口吃了进去。<br>在你还在回味那有些惊悚的一幕时，强化作业已经开始了。<br>……<br>在纺织者的强化下，<span class=\"yellow b\">$arb</span>的防御力变成了<span class=\"yellow b\">$arbe</span>。<br>";
+					$itme = 0;
+				}
 			}
 			return;
 		}
