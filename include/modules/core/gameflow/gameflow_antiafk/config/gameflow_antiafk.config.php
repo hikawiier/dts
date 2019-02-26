40818f61780eebd74ce156774b7b4c8de3e63baf
7 <hikawiier@foxmail.com>
2019-02-26 23:33:51 +0800

    梳理一下思路 02.26

--- include/modules/core/gameflow/gameflow_antiafk/config/gameflow_antiafk.config.php
+++ include/modules/core/gameflow/gameflow_antiafk/config/gameflow_antiafk.config.php
@@ -3,9 +3,9 @@
 namespace gameflow_antiafk
 {
 	//标准房反挂机系统间隔时间，单位分钟
-	$antiAFKertime_normal = 10;
+	$antiAFKertime_normal = 30;
 	//房间内反挂机系统间隔时间，单位分钟
-	$antiAFKertime_room = 10;
+	$antiAFKertime_room = 30;
 }
 
 ?>
