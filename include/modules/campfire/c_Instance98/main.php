f6a36f8632f0259e0b8fca8a8bd42ace5842bc93
7 <hisuinohoshi@gmail.com>
2022-12-13 08:45:50 +0800

    前期准备
    要开始给饼填馅了

---
+++ include/modules/campfire/c_Instance98/main.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace instance98
+{
+	function init() {
+		eval(import_module('skillbase'));
+		$valid_skills[98] = array(3000);
+	}
+	
+	function get_npclist(){
+		if (eval(__MAGIC__)) return $___RET_VALUE; 
+		eval(import_module('sys','instance98'));
+		if (98 == $gametype){
+			return $npcinfo_instance98;
+		}else return $chprocess();
+	}
+	
+	function get_itemfilecont(){
+		if (eval(__MAGIC__)) return $___RET_VALUE; 
+		eval(import_module('sys'));
+		if (98 == $gametype){
+			$file = __DIR__.'/config/mapitem.config.php';
+			$l = openfile($file);
+			return $l;
+		}else return $chprocess();
+	}
+	
+	function get_startingitemfilecont(){
+		if (eval(__MAGIC__)) return $___RET_VALUE; 
+		eval(import_module('sys'));
+		if (98 == $gametype){
+			$file = __DIR__.'/config/stitem.config.php';
+			$l = openfile($file);
+			return $l;
+		}else return $chprocess();
+	}
+	
+	function get_startingwepfilecont(){
+		if (eval(__MAGIC__)) return $___RET_VALUE; 
+		eval(import_module('sys'));
+		if (98 == $gametype){
+			$file = __DIR__.'/config/stwep.config.php';
+			$l = openfile($file);
+			return $l;
+		}else return $chprocess();
+	}
+	
+	function get_trapfilecont(){
+		if (eval(__MAGIC__)) return $___RET_VALUE; 
+		eval(import_module('sys'));
+		if (98 == $gametype){
+			$file = __DIR__.'/config/trapitem.config.php';
+			$l = openfile($file);
+			return $l;
+		}else return $chprocess();
+	}
+	
+}
+
+?>
